4e0eead48af0abea757cf305ab13d3ea51bfc3c6
mhd adnan alkharfan <55524560+adnan123k@users.noreply.github.com>
2023-03-16 13:41:45 +0300

    fixed static quote

--- BMI Calculator/Controllers/ViewController2.swift
+++ BMI Calculator/Controllers/ViewController2.swift
@@ -13,6 +13,28 @@ class ViewController2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         res.text=bmi
+        if(Double(bmi)!<18.5){
+            label.text="eat more"
+            
+        }
+        else if((18.5<=Double(bmi)!) && (Double(bmi)!<=24.9)){
+            label.text="Congratulations on having a healthy weight"
+        }
+        else if((25<=Double(bmi)!) && (Double(bmi)!<=29.9)){
+            label.text="You have to lose some weigh"
+        }
+        else if((30<=Double(bmi)!) && (Double(bmi)!<=34.9)){
+            label.text="You should seek medical help to lose weight"
+            label.font = UIFont(name: "Helvetica Neue", size: UIFont.preferredFont(forTextStyle: .body).pointSize)
+        }
+        else if((35<=Double(bmi)!) && (Double(bmi)!<=39.9)){
+            label.text="You need urgent medical attention to lose weight"
+            label.font = UIFont(name: "Helvetica Neue", size: UIFont.preferredFont(forTextStyle: .body).pointSize)
+        }
+        else{
+            label.text="You need immediate intervention to lose weight"
+            label.font = UIFont(name: "Helvetica Neue", size: UIFont.preferredFont(forTextStyle: .body).pointSize)
+        }
         // Do any additional setup after loading the view.
     }
     var bmi=""
